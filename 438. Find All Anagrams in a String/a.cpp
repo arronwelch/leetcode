bec700967bf4dde79a1f208c3e097f440b40a198
nick <Davego@protonmail.com>
2022-11-29 10:43:00 +0800

    add solution2

--- 438. Find All Anagrams in a String/a.cpp
+++ 438. Find All Anagrams in a String/a.cpp
@@ -32,7 +32,7 @@ Constraints:
 
 using namespace std;
 
-class Solution
+class Solution1
 {
 public:
 	vector<int> findAnagrams(string s, string p)
@@ -68,15 +68,77 @@ public:
 	}
 };
 
+class Solution2 {
+public:
+	vector<int> findAnagrams(string s, string p) {
+		int sLen = s.size(), pLen = p.size();
+
+		if (sLen < pLen) {
+			return vector<int>();
+		}
+
+		vector<int> ans;
+		vector<int> count(26);
+		for (int i = 0; i < pLen; ++i) {
+			++count[s[i] - 'a'];
+			--count[p[i] - 'a'];
+		}
+	
+		int differ = 0;
+		for (int j = 0; j < 26; ++j) {
+			if (count[j] != 0) {
+				++differ;
+			}
+		}
+
+		if (differ == 0) {
+			ans.emplace_back(0);
+		}
+
+		for (int i = 0; i < sLen - pLen; ++i) {
+			if (count[s[i] - 'a'] == 1) {
+				--differ;
+			} else if (count[s[i] - 'a'] == 0) {
+				++differ;
+			}
+			--count[s[i] - 'a'];
+
+			if (count[s[i + pLen] - 'a'] == -1) {
+				--differ;
+			} else if (count[s[i + pLen] - 'a'] == 0) {
+				++differ;
+			}
+			++count[s[i + pLen] - 'a'];
+
+			if (differ == 0) {
+				ans.emplace_back(i + 1);
+			}
+		}
+		return ans;
+	}
+};
+
 int main()
 {
-	string s = "abcccbad";
-	string p = "abc";
+// case 1:
+//	string s = "cbaebabacd";
+//	string p = "abc";
 
-	Solution a;
-	vector<int> ans = a.findAnagrams(s, p);
+// case 2:
+	string s = "abab";
+	string p = "ab";
+
+	Solution1 a1;
+	vector<int> ans1 = a1.findAnagrams(s, p);
 	cout << "[ ";
-	for(auto it = ans.begin(); it != ans.end(); ++it)
+	for(auto it = ans1.begin(); it != ans1.end(); ++it)
+		cout << *it << ", ";
+	cout << "\b\b ]" << endl;
+
+	Solution2 a2;
+	vector<int> ans2 = a2.findAnagrams(s, p);
+	cout << "[ ";
+	for(auto it = ans2.begin(); it != ans2.end(); ++it)
 		cout << *it << ", ";
 	cout << "\b\b ]" << endl;
 }
