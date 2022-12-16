cdc4b96fb4b7d764a2ef0e30f3aac52c4ed05796
nick <Davego@protonmail.com>
2022-12-16 14:15:00 +0800

    reverse linked list

---
+++ 206. Reverse Linked List/a.cpp
@@ -0,0 +1,98 @@
+/* 206. Reverse Linked List */
+
+/* solution A */
+
+/*
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+Constraints:
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+*/
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* nptr) : val(x), next(nptr) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        while(cur)
+        {
+            ListNode* next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+};
+
+int main()
+{
+    ListNode* note = new ListNode(1);
+    ListNode* head = note;
+    note->next = new ListNode(2);
+    note = note->next;
+    note->next = new ListNode(3);
+    note = note->next;
+    note->next = new ListNode(4);
+    note = note->next;
+    note->next = new ListNode(5);
+
+    cout << "before:\n";
+    ListNode* temp = head;
+    while(temp)
+    {
+        cout << temp->val << ' ';
+        temp = temp->next;
+    }
+    cout << endl;
+
+    Solution A;
+    ListNode* ans = A.reverseList(head);
+    cout << "after:\n";
+    while(ans)
+    {
+        cout << ans->val << ' ';
+        ans = ans->next;
+    }
+    cout << endl;
+}
